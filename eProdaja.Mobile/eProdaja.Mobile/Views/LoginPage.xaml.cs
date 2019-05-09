78496ce06e3ee067b3695bf5aca914a3abaa5d35
Amel Musić <amel.music@gmail.com>
2019-05-09 16:42:09 +0200

    Revert "Revert "V8""
    This reverts commit 847aaff5bc032574b86f120ed698777e5d4ea880.

---
+++ eProdaja.Mobile/eProdaja.Mobile/Views/LoginPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace eProdaja.Mobile.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+		}
+
+	    private void Entry_OnTextChanged(object sender, TextChangedEventArgs e)
+	    {
+	        
+	    }
+	}
+}
--- eProdaja.WinUI/Korisnici/frmKorisnici.Designer.cs
+++ eProdaja.WinUI/Korisnici/frmKorisnici.Designer.cs
@@ -31,14 +31,14 @@
             this.btnPrikazi = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.dgvKorisnici = new System.Windows.Forms.DataGridView();
-            this.txtPretraga = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.KorisnikID = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.KorisnickoIme = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Ime = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Prezime = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Status = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.txtPretraga = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvKorisnici)).BeginInit();
             this.SuspendLayout();
@@ -46,7 +46,7 @@
             // btnPrikazi
             // 
             this.btnPrikazi.Location = new System.Drawing.Point(936, 33);
-            this.btnPrikazi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPrikazi.Margin = new System.Windows.Forms.Padding(4);
             this.btnPrikazi.Name = "btnPrikazi";
             this.btnPrikazi.Size = new System.Drawing.Size(100, 28);
             this.btnPrikazi.TabIndex = 0;
@@ -58,9 +58,9 @@
             // 
             this.groupBox1.Controls.Add(this.dgvKorisnici);
             this.groupBox1.Location = new System.Drawing.Point(17, 84);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox1.Size = new System.Drawing.Size(1019, 455);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
@@ -80,7 +80,7 @@
             this.Status});
             this.dgvKorisnici.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvKorisnici.Location = new System.Drawing.Point(4, 19);
-            this.dgvKorisnici.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvKorisnici.Margin = new System.Windows.Forms.Padding(4);
             this.dgvKorisnici.Name = "dgvKorisnici";
             this.dgvKorisnici.ReadOnly = true;
             this.dgvKorisnici.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
@@ -88,24 +88,6 @@
             this.dgvKorisnici.TabIndex = 0;
             this.dgvKorisnici.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvKorisnici_CellContentClick);
             this.dgvKorisnici.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvKorisnici_CellDoubleClick);
-            // 
-            // txtPretraga
-            // 
-            this.txtPretraga.Location = new System.Drawing.Point(21, 36);
-            this.txtPretraga.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txtPretraga.Name = "txtPretraga";
-            this.txtPretraga.Size = new System.Drawing.Size(432, 22);
-            this.txtPretraga.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 11);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 17);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Pretraga";
             // 
             // KorisnikID
             // 
@@ -150,6 +132,24 @@
             this.Status.Name = "Status";
             this.Status.ReadOnly = true;
             // 
+            // txtPretraga
+            // 
+            this.txtPretraga.Location = new System.Drawing.Point(21, 36);
+            this.txtPretraga.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPretraga.Name = "txtPretraga";
+            this.txtPretraga.Size = new System.Drawing.Size(432, 22);
+            this.txtPretraga.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pretraga";
+            // 
             // frmKorisnici
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -159,7 +159,7 @@
             this.Controls.Add(this.txtPretraga);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnPrikazi);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmKorisnici";
             this.Text = "frmKorisnici";
             this.groupBox1.ResumeLayout(false);
